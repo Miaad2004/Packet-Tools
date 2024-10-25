98a777ad7f79416445dd7021e71f6e50ae9ac1ba
Miaad <Miaad@MIAAD>
2024-10-25 23:09:55 +0330

    implemented scan_port

--- nmap_clone.py
+++ nmap_clone.py
@@ -2,19 +2,32 @@ import socket
 import time
 import argparse
 import threading
+from concurrent.futures import ThreadPoolExecutor
 
 class NetworkMapper:
     def __init__(self):
         pass 
-        self.ttl = 0.5
+        self.conn_timeout = 
     
     @staticmethod
     def is_online(host):
         # Should check icmp + ports 1 to 1024
         pass
     
-    def scan_port(host, port, result, mode="TCP"):
+    def scan_port(self, host, port, mode="TCP"):
         assert mode in ["TCP", "UDP"]
+        assert port >= 0 and port <= 2**16
+        
+        socket_type = socket.SOCK_STREAM if mode == "TCP" else socket.SOCK_DGRAM
+        with socket.socket(socket.AF_INET, socket_type) as s:
+            s.settimeout(self.conn_timeout)
+            start_time = time.time()
+            is_open = s.connect_ex((host, port)) == 0
+            latency = time.time() - start_time if is_open else None
+        
+        return [is_open, latency]
+    
+    def calc_average_latency(self, host, port, n=100, mode="TCP"):        
         # scans a single port if open returns the latencies
         pass
     
