b2aab797e7f6e818431425df25d679ceb90d1b45
Miaad <Miaad@MIAAD>
2024-11-03 18:32:03 +0330

    implement DNS header

--- DNS.py
+++ DNS.py
@@ -14,3 +14,35 @@ class QueryType(enum.Enum):
     SOA = 6     # Start of authority
     PTR = 12    # Pointer record
     TXT = 16    # Text record
+
+# Based on https://mislove.org/teaching/cs4700/spring11/handouts/project1-primer.pdf
+class DNS:
+    """
+    DNS packet structure:
+    ----------------------
+    |       Header       |      
+    ----------------------
+    |      Question      |
+    ----------------------
+    |       Answer       |
+    ----------------------
+    |      Authority     |
+    ----------------------
+    |      Additional    |
+    ----------------------
+    """
+    def __init__(self):
+        self.transaction_id = random.randint(0, 2**15 - 1) # 16 bits
+        self.QR = 0     # Query or Response | 1 bit
+        self.opcode = 0 # Query Type | 4 bits 
+        self.AA = 0     # Authoritative Answe | 1 bit
+        self.TC = 0     # 1 bit
+        self.RD = 1     # Recursion Desired | 1 bit
+        self.RA = 0     # Recursion Available | 1 bit
+        self.Z = 0      # For future use | 1 bit
+        self.RCODE = 0  # Response Code | 1 bit
+        self.n_queries = 1 # 16 bits
+        self.n_answers = 0 # 16 bits
+        self.ns_count = 0  # Number of name servers in Authority Section  | 16 bits
+        self.na_count = 0  # Number of additional records (usually A record of name servers) | 16 bits
+    
